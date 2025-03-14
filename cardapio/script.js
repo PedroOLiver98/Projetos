70ddce3b30d4f65ad0a90dabc49665c26d18811b
PedroOliver98 <skrsofficial@gmail.com>
2025-03-13 21:55:56 -0300

    atualizacao do cardapio

--- cardapio/script.js
+++ cardapio/script.js
@@ -1,3 +1,4 @@
+import cardapio from './dados.js'
 /*
 <h2>Cardapio do Dia</h2>
 <h3>Segunda-feira</h3>
@@ -23,4 +24,31 @@ const diaDaSemana = ['domingo','segunda-feira','terça-feira','quarta-feira','qu
 const hoje = new Date().getDay(); //Date: Informa a data e getDay(): Informa qual dos 7 (de 0 a 6)
 
 const diaAtual = diaDaSemana[hoje]
-    alert(diaAtual)
+
+const menuDoDia=cardapio.find (menu=>menu.dia ===diaAtual);
+console.log(menuDoDia)
+    if(menuDoDia){
+        const h3=document.createElement('h3');
+        h3.textContent=menuDoDia.dia;
+        main.appendChild(h3);
+        
+        const section=document.createElement('section');
+        const ul=document.createElement('ul');
+        
+        menuDoDia.cardapio.split(', ').forEach(item => {
+            const li=document.createElement('li');
+            li.textContent=item;
+            ul.appendChild(li);    
+        });
+        section.appendChild(ul);
+        const figure=document.createElement('figure');
+        const img=document.createElement('img');
+        img.src=menuDoDia.img;
+        img.alt=menuDoDia.alt;
+
+        figure.appendChild(img);
+        section.appendChild(figure);
+        main.appendChild(section);
+    }else{
+        main.innerHTML='<h2>Hoje não temos cardapio disponivel</h2>'
+    }
