314b16166f51ce9777e35a1eae3b203ccc22b1d2
PedroOliver98 <skrsofficial@gmail.com>
2025-03-20 20:55:57 -0300

    adicionado rodapé e mais palavras no banco de dados

--- jogo_da_forca_v2/words.js
+++ jogo_da_forca_v2/words.js
@@ -1,3 +1,11 @@
+/*
+Dica:
+
+word é a Palavra
+clue é a dica sobre a palavra
+
+*/
+
 const words = [
   { word: "abacaxi", clue: "Fruta tropical" },
   { word: "elefante", clue: "Animal de grande porte com tromba longa" },
@@ -19,7 +27,35 @@ const words = [
   { word: "raios", clue: "Descargas elétricas na atmosfera" },
   { word: "montanha", clue: "Elevação natural da terra" },
   { word: "vela", clue: "Objeto para iluminação" },
-  { word: "otorrinolaringologista", clue: "Garganta"}
+  { word: "otorrinolaringologista", clue: "Garganta"},
+  { word: "carro", clue: "Possui rodas"},
+  { word: "macaco", clue: "levantar pesos"},
+  { word: "datena", clue: "arremessar cadeiras"},
+  { word: "vassoura", clue: "uso doméstico"},
+  { word: "capacete", clue: "usa na cabeça"},
+  { word: "ventilador", clue: "refresca o ambiente"},
+  { word: "som", clue: "produz ruidos"},
+  { word: "dinossauro", clue: "baby"},
+  { word: "pernilongo", clue: "vampiro noturno"},
+  { word: "tempestade", clue: "clima"},
+  { word: "piscina", clue: "lazer"},
+  { word: "eslovaquia", clue: "país"},
+  { word: "encaracolado", clue: "cabelo"},
+  { word: "luz", clue: "iluminação"},
+  { word: "esmalte", clue: "unha"},
+  { word: "pitaya", clue: "fruta"},
+  { word: "camburão", clue: "policia"},
+  { word: "vermelho", clue: "cor"},
+  { word: "fisioterapeuta", clue: "profissão"},
+  { word: "programador", clue: "informática"},
+  { word: "cabaré", clue: "cerveja"},
+  { word: "caderno", clue: "escola"},
+  { word: "onibus", clue: "transporte"},
+  { word: "outono", clue: "clima"},
+  { word: "atletico", clue: "galo"},
+  { word: "cabeça", clue: "parte do corpo"},
+
+
 ];
 
 export default function getWord() {
